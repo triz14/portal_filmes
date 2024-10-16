4146e71a21f60f3985651e4ce3aabccf16708997
triz14 <beatriznovais1404@gmail.com>
2024-10-15 21:27:58 -0300

    Chamada de api

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 
-export default function MovieCard({titulo, id, imagem_destaque}) {
+export default function MovieCard({id, title, backdrop_path, poster_path}) {
     return(
         <div>
-            <h2>{titulo}</h2>
-            <img src={`/${imagem_destaque}`} alt="" />
+            <h2>{title}</h2>
+            <img src={`https://image.tmdb.org/t/p/w1280${backdrop_path}`} />
+            <img src={`https://image.tmdb.org/t/p/w154${poster_path}`} />
             <Link to={`movies/${id}`}>Saiba mais</Link>
         </div>
     )
--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,16 +1,24 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import MovieCard from "../components/MovieCard"
-import movies from "../data/movies.json"
 
 export default function MovieListPage() {
 
     const [search, setSearch] = useState('')
+    const [filmes, setFilmes] = useState([])
+
+    useEffect( () => {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=7c572a9f5b3ba776080330d23bb76e1e&language=pt-br')
+        .then( results => results.json())
+        .then( data => setFilmes(data.results))
+        .catch( erro => console.log(erro))
+        .finally( () => console.log("Cabo!!"))
+    }, [])
 
     const handleSearch = (e) => {
         setSearch(e.target.value)
     }
 
-    const filmesFiltrados = movies.filter(filme => filme.titulo.toLowerCase().includes(search.toLowerCase()))
+    const filmesFiltrados = filmes.filter(filme => filme.title.toLowerCase().includes(search.toLowerCase()))
 
     return (
         <>
@@ -24,7 +32,7 @@ export default function MovieListPage() {
                 onChange={handleSearch}
             />
 
-            <div className="flex">
+            <div>
                 {
                     filmesFiltrados.length > 0 ?
                         filmesFiltrados
